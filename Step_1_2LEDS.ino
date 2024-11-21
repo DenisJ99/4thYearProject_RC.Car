28c88e35721807c7ab7b0061184e86884aff9fd0
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-11-21 18:09:01 +0000

    Add files via upload

---
+++ Step_1_2LEDS.ino
@@ -0,0 +1,21 @@
+// Define the LED pin
+const int RLed = 4;
+const int GLed = 23;
+
+void setup() {
+  // Initialize the LED pin as an output
+  pinMode(RLed, OUTPUT);
+  pinMode(GLed, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on
+  digitalWrite(GLed, HIGH);
+  digitalWrite(RLed, HIGH);
+  delay(1000); // Wait for 1 second
+
+  // Turn the LED off
+  digitalWrite(GLed, LOW);
+  digitalWrite(RLed, LOW);
+  delay(1000); // Wait for 1 second
+}
